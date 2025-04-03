b82d794be182854d3df66c45bba8ec131d50f24b
Dayton Hannaford <144299432+daytonhannaforddev@users.noreply.github.com>
2025-04-03 17:40:00 -0400

    Upload JavaDoc

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.AchievementManagerMaster"},{"l":"org.AchievementManagerMaster.GUI"}];updateSearchResults();
---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.AchievementManagerMaster","l":"DatabaseManager"},{"p":"org.AchievementManagerMaster","l":"GameManager"},{"p":"org.AchievementManagerMaster.GUI","l":"GameManagerGUI"},{"p":"org.AchievementManagerMaster","l":"Main"},{"p":"org.AchievementManagerMaster","l":"VideoGame"}];updateSearchResults();
